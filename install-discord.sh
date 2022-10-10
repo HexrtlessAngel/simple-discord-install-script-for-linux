8aa79ed6189068913c39bbaf03393217a89daf17
HexrtlessAngel <95123544+HexrtlessAngel@users.noreply.github.com>
2022-10-10 18:58:07 -0400

    Update install-discord.sh

--- install-discord.sh
+++ install-discord.sh
@@ -3,20 +3,20 @@ sudo echo "Hello! :D"
 echo "Killing any currently running discord instance."
 killall discord
 killall Discord
-echo "Downloading latest Discord (Native) tar.gz..."
+echo "Downloading latest Discord tar.gz..."
 wget -O discord.tar.gz "https://discord.com/api/download?platform=linux&format=tar.gz"
 tar xf discord.tar.gz
 rm discord.tar.gz
-sudo echo "Installing Discord (Native)..."
+sudo echo "Installing Discord..."
 sudo rm -rf /usr/share/discord
 sudo cp -r Discord/ /usr/share/discord
-echo "Discord (Native) adding shortcuts..."
+echo "Discord adding shortcuts..."
 sudo cp discord/discord.desktop /usr/share/applications/discordnative.desktop
 cp Discord/discord.desktop "/home/$(whoami)/Desktop/discord.desktop"
 sudo rm /usr/bin/discord
-echo "Discord (Native) adding terminal commands..."
+echo "Discord adding terminal commands..."
 sudo ln -sf /usr/share/discord/Discord /usr/bin/discord
-echo "Discord (Native) installed!"
+echo "Discord installed!"
 echo "Cleaning up some shit..."
 rm -rf Discord
 discord & disown
